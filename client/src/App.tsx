d1ab49b358772beb2715cbe8d604331565018c35
Filipe Silva <filipeqs@outlook.com>
2023-05-27 14:14:59 -0400

    showing products on screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,25 @@
+const products = [
+  {
+    name: 'product1',
+    price: 100.0,
+  },
+  {
+    name: 'product2',
+    price: 200.0,
+  },
+];
+
 function App() {
   return (
     <div>
       <h1>Re-Store</h1>
+      <ul>
+        {products.map((item, i) => (
+          <li key={i}>
+            {item.name} - {item.price}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
