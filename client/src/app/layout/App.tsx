07a526f256e7c90f746e809b116d704a9384178b
Filipe Silva <filipeqs@outlook.com>
2023-05-30 18:50:10 -0400

    adding styling with material UI

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import { Product } from '../models/products';
 import Catalog from '../../features/catalog/Catalog';
-import { Typography } from '@mui/material';
+import Header from './Header';
+import { Container, CssBaseline } from '@mui/material';
 
 function App() {
   const [products, setProduct] = useState<Product[]>([]);
@@ -28,8 +29,11 @@ function App() {
 
   return (
     <>
-      <Typography variant="h1">Re-Store</Typography>
-      <Catalog products={products} addProduct={addProduct} />
+      <CssBaseline />
+      <Header />
+      <Container>
+        <Catalog products={products} addProduct={addProduct} />
+      </Container>
     </>
   );
 }
